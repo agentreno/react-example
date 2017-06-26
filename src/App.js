2f6f513e3f31768e4eb43b48e8ed749639e96112
Karl <kturrell@hotmail.com>
2017-06-26 07:58:19 +0100

    Add state to component
    Shows that setState doesn't overwrite other state values if they aren't in the
    object passed in. Causes re-rendering so DOM is updated as soon as values are
    entered into the input field.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            stateMessage: 'default state',
+            unchangedMessage: 'unchanged'
+        }
+    }
+
+    update(e) {
+        this.setState({
+            stateMessage: e.target.value
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.props.topMessage}</h1>
                 <h2>{this.props.bottomMessage}</h2>
+                <h3>{this.state.stateMessage} - {this.state.unchangedMessage}</h3>
+                <input type="text" onChange={this.update.bind(this)} />
             </div>
         )
     }
@@ -20,4 +36,4 @@ App.defaultProps = {
     topMessage: 'hello world'
 }
 
-export default App;
+export default App
