42cbefa9343c7aa8df5d3dfcdde491c78d233cd2
Karl <kturrell@hotmail.com>
2017-06-26 08:23:00 +0100

    Demonstrate custom prop validation

--- src/App.js
+++ src/App.js
@@ -37,7 +37,15 @@ const Button = (props) => <button>{props.children}</button>
 
 
 App.propTypes = {
-    topMessage: React.PropTypes.string.isRequired,
+    //topMessage: React.PropTypes.string.isRequired,
+    topMessage(props, propName, component) {
+        if (!(propName in props)) {
+            return new Error(`missing ${propName} prop`)
+        }
+        if (props[propName].length < 6) {
+            return new Error(`${propName} is too short`)
+        }
+    },
     bottomMessage: React.PropTypes.string
 }
 
