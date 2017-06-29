032cf91383f0c171381edccffe6b681ad75a600b
Karl <kturrell@hotmail.com>
2017-06-29 21:44:40 +0100

    Remove monolithic top level example

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <App topMessage='hello one' bottomMessage='hello two' />,
-  document.getElementById('root')
-);
