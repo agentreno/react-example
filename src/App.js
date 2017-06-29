8bf931a2237502dce71738be4707dde9c6dd6dc5
Karl <kturrell@hotmail.com>
2017-06-29 07:53:27 +0100

    Add component lifestyle methods example

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
                 <Button>I <Hearts /> React</Button>
                 <DemoEvents />
                 <DemoRefs />
+                <Wrapper />
             </div>
         )
     }
@@ -121,6 +122,57 @@ class DemoRefs extends React.Component {
                     ref={component => this.c = component}
                     update={this.update}
                 /> {this.state.c}
+            </div>
+        )
+    }
+}
+
+class ButtonCounter extends React.Component {
+    constructor() {
+        super()
+        this.state = {val: 0}
+        this.update = this.update.bind(this)
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount')
+    }
+
+    update() {
+        this.setState({val: this.state.val+1})
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+    render() {
+        console.log('render')
+        return (
+            <button onClick={this.update}>{this.state.val}</button>
+        )
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+    }
+
+}
+
+class Wrapper extends React.Component {
+    mount() {
+        ReactDOM.render(<ButtonCounter />, document.getElementById('container'))
+    }
+
+    unmount() {
+        ReactDOM.unmountComponentAtNode(document.getElementById('container'))
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.mount.bind(this)}>Mount</button>
+                <button onClick={this.unmount.bind(this)}>Unmount</button>
+                <div id="container"></div>
             </div>
         )
     }
