4c576a8db7293b3c4cd84bf83965bb26bbf88c2d
Karl <kturrell@hotmail.com>
2017-06-30 16:31:35 +0100

    Add children iteration example to props-children

--- props-children/src/App.js
+++ props-children/src/App.js
@@ -5,6 +5,10 @@ class App extends React.Component {
         return (
             <div>
                 <Button>I <Hearts /> React</Button>
+                <Parent>
+                    <Hearts className="childA" />
+                    <Hearts className="childB" />
+                </Parent>
             </div>
         )
     }
@@ -13,5 +17,23 @@ class App extends React.Component {
 const Hearts = () => <span>&hearts;</span>
 
 const Button = (props) => <button>{props.children}</button>
+
+class Parent extends React.Component {
+    render() {
+        // Children starts as a React object
+        console.log(this.props.children)
+       
+        // React.Children defines a forEach (first arg is the children prop)
+        let items = React.Children.forEach(this.props.children,
+            child => console.log(child.props.className)
+        )
+
+        // And a toArray for when the children just need storing
+        items = React.Children.toArray(this.props.children)
+        console.log(items)
+
+        return null
+    }
+}
 
 export default App
